a50adbae334a916c2e327a67921d053adaa2ad82
jrs33 <surettej@bu.edu>
2016-11-30 20:20:08 -0500

    Commiting the following changes; Firebase linkage, class creation for the edit and create new club activities, functionality added to new club (ie: ability to write to Firebase)

--- app/src/main/java/com/example/abigail/clubconnecthomepage/MainActivity.java
+++ app/src/main/java/com/example/abigail/clubconnecthomepage/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.abigail.clubconnecthomepage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // activity_main is the id for the homepage
+
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent i = new Intent(v.getContext(), NewClubActivity.class);
+                v.getContext().startActivity(i);
+            }
+        });
+
     }
+
 }
+
